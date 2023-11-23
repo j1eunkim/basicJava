a10fc849340aaf05d5e4eaf51d45bcaab7894389
j1eunkim <jijinn0517@naver.com>
2023-11-23 18:11:18 +0900

    클래스 형변환

--- firstProject/src/firstProject/Bus.java
+++ firstProject/src/firstProject/Bus.java
@@ -4,4 +4,8 @@ public class Bus extends Car {
 	public void noise() {
 		System.out.println("빵빵");
 	}
+	public void run() {
+		super.run();//부모의 run을 실행하려면 super생성자를 이용
+		System.out.println("자식이 run");
+	} 
 }
--- firstProject/src/firstProject/BusEx.java
+++ firstProject/src/firstProject/BusEx.java
@@ -1,13 +1,17 @@
-package firstProject;
+ package firstProject;
 
 public class BusEx {
 	public static void main(String[] args) {
 		Bus bus = new Bus();
+		Car c = new Bus();//이렇게 가리키는 것도 가능 
 		bus.run(); //Car로부터 상속받은 메소드
 		bus.noise();
 		
 		Car car = new Car();
 		car.run();
 		//car.noise();는 안됨. 부모클래스가 자삭이 갖고있는 메소드는 사용할 수 없어서
+		
+		Bus bus2 = (Bus)c; //클래스 형변환
+		bus2.noise();
 	}
 }
